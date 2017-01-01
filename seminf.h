8c09f86bd30fd3077d97e837f4776ae0d883b3d0
awsteiner <awsteiner@mykolab.com>
2017-01-01 16:54:18 -0500

    Updated copyright years.

--- seminf.h
+++ seminf.h
@@ -1,7 +1,7 @@
 /*
   -------------------------------------------------------------------
   
-  Copyright (C) 2002-2016, Andrew W. Steiner
+  Copyright (C) 2002-2017, Andrew W. Steiner
   
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
